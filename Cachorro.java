8b6786021ce807127fe89562ef388b0e942eb276
MayconFelipeBianchi <114965402+MayconFelipeBianchi@users.noreply.github.com>
2023-03-08 20:23:32 -0300

    Exercicio
    Criar uma classe distinguindo 5 atributos de um animal e depois fazer uma classe para cadastrar 5 animais.

---
+++ Cachorro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject5;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cachorro {
+          public String nome;
+          public int peso;
+          public String corOlhos;
+          public int quantPatas;
+      }
+
---
+++ Mavenproject5.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject5;
+
+/**
+ *
+ * @author aluno
+ */
+public class Mavenproject5 {
+         public static void main(String[] args) {
+             Cachorro cachorro1 = new Cachorro();
+             cachorro1.nome = "Bolinha";
+             cachorro1.corOlhos = "azuis";
+             cachorro1.peso = 30;
+             cachorro1.quantPatas = 4;
+                        
+             Cachorro cachorro2 = new Cachorro();
+             cachorro2.nome = "Rex";
+             cachorro2.corOlhos = "verde";
+             cachorro2.peso = 50;
+             cachorro2.quantPatas = 4;
+
+             Cachorro cachorro3 = new Cachorro();
+             cachorro3.nome = "Tom";
+             cachorro3.corOlhos = "verde";
+             cachorro3.peso = 40;
+             cachorro3.quantPatas = 4;
+                        
+             Cachorro cachorro4 = new Cachorro();
+             cachorro4.nome = "Jucelino";
+             cachorro4.corOlhos = "azuis";
+             cachorro4.peso = 60;
+             cachorro4.quantPatas = 4;
+                       
+             Cachorro cachorro5 = new Cachorro();
+             cachorro5.nome = "Jusevaldo";
+             cachorro5.corOlhos = "marrom";
+             cachorro5.peso = 50;
+             cachorro5.quantPatas = 4;
+                        
+       
+    }
+}
+
